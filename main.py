ce6fdd3fe78d3ba44ba0c1462ef014f5813e92d9
demka06 <59875707+demka06@users.noreply.github.com>
2024-10-28 23:24:58 +0300

    Add files via upload
    Переход на asyncio. Разбиение сложных методов на более простые.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 from os import mkdir
 from os import listdir
 from os.path import isdir
@@ -5,11 +6,12 @@ from typing import List
 
 import eyed3
 from loguru import logger
-from telebot import TeleBot
+from telebot.async_telebot import AsyncTeleBot
 from telebot import apihelper
-from yandex_music import Client
+from yandex_music import ClientAsync
 from yandex_music import exceptions
 from yandex_music.track.track import Track
+from yandex_music.tracks_list import TracksList
 
 # Зависимости телеграм
 TELEGRAM_ID = 0
@@ -25,7 +27,8 @@ BITRATES = [64, 128, 192, 320]
 eyed3.log.setLevel("ERROR")
 
 
-def set_tags(track: Track, file: str) -> None:
+async def set_tags(track: Track,
+                   file: str) -> None:
     """Добавление аудиофайлам исполнителя, названия, альбома, нумерации
 
     Args:
@@ -33,28 +36,23 @@ def set_tags(track: Track, file: str) -> None:
         file (str): Путь до сохраненного трека
     """
 
-    # Извлечение необхордимых переменных
-    track_name = track["title"]
-    track_artists = ", ".join([i["name"] for i in track["artists"]])
-    album = track["albums"][0]["title"]
-    track_count = track["albums"][0]["track_count"]
-
     logger.debug("Выгрузка тегов")
     audiofile = eyed3.load(path=file)  # Выгрузка тегов
     audiofile.initTag()  # Инициализация тегов
 
     # Присвоение новых значений тегам
     file_tag = audiofile.tag
-    file_tag.title = track_name
-    audiofile.tag.artist = track_artists
-    file_tag.album = album
-    file_tag.track_num = track_count
+    file_tag.title = track.title
+    audiofile.tag.artist = ", ".join([i["name"] for i in track.artists])
+    file_tag.album = track.albums[0].title
+    file_tag.track_num = track.albums[0].track_count
 
     logger.debug("Сохранение тегов")
     file_tag.save()  # Сохранение тегов
 
 
-def get_downloaded_tracks(dir_name: str = "tracks", codec: str = "mp3") -> List:
+async def get_downloaded_tracks(dir_name: str = "tracks",
+                                codec: str = "mp3") -> List:
     """Получение уже загруженных треков
 
     Args:
@@ -74,7 +72,9 @@ def get_downloaded_tracks(dir_name: str = "tracks", codec: str = "mp3") -> List:
     return [file for file in files if file.endswith(codec)] if files is not None else []
 
 
-def send_all_tracks_to_telegram(dir_name: str = "tracks", count: int = 10, offset: int = 0):
+async def send_all_tracks_to_telegram(dir_name: str = "tracks",
+                                      count: int = 10,
+                                      offset: int = 0):
     """Отправка всех треков в телеграм
 
     Args:
@@ -84,54 +84,93 @@ def send_all_tracks_to_telegram(dir_name: str = "tracks", count: int = 10, offse
             offset (int): сдвиг
     """
 
-    audio_files = get_downloaded_tracks(dir_name)  # Список треков
+    audio_files = await get_downloaded_tracks(dir_name)  # Список треков
     bot = Telegram()  # Инициализация бота
-    counter = 0  # Управляющая переменная
 
+    # Отправление треков
+    [await bot.send_audio_from_file(path=f"./{dir_name}/{file}") for file in audio_files[offset:count]]
+
+    """
+    counter = 0  # Управляющая переменная
+    
     # Перебор и отправление треков
     for file in audio_files[offset:]:
-
+    
         # Выход из цикла по счетчику
         if counter >= count:
             break
-
-        bot.send_audio(f"./{dir_name}/{file}")
-
+    
+        await bot.send_audio_from_file(path=f"./{dir_name}/{file}")
+    
         counter += 1  # Увеличение счетчика
+    """
 
 
 class YandexMusic:
-    """Упрощение взаимодействия с модулем yandex_music
+    """Упрощение взаимодействия с модулем yandex_music.
 
     Args:
         token (str): Токен для авторизации в https://api.music.yandex.net
     """
 
-    def __init__(self, token: str = YANDEX_TOKEN):
+    def __init__(self, token: str = YANDEX_TOKEN) -> None:
         try:
             logger.debug("Авторизация")
-            self.client = Client(token).init()  # Авторизация
+            self.client = ClientAsync(token)  # Авторизация
         except exceptions.UnauthorizedError:
             logger.error("Недействительный токен")
 
-    def download_tracks(self, dir_name: str = "tracks", codec: str = 'mp3', bitrate: int = 192,
-                        count: int = 10, offset: int = 0) -> None:
-        """Получение и сохранение понравившихся треков
+    async def __init_client(self):
+        """Получение информацию об аккаунте использующихся в других запросах."""
+
+        await self.client.init()
+
+    async def get_tracks(self, track_ids: List[str] | List[int] | List[str | int] | int | str = 0,
+                         count: int = 10,
+                         offset: int = 0) -> list[Track] | TracksList | None:
+        """
+        Получение информации о треке.
+
+        Args:
+            track_ids (:obj:`str` | :obj:`int` | :obj:`list` из :obj:`str` | :obj:`list` из :obj:`int`): Уникальный
+            идентификатор трека или треков. Если track_ids = 0, то отдаст все лайкнутые треки
+            count (int): количество треков, которые нужно получить
+            offset (int): сдвиг
+        """
+
+        await self.__init_client()
+
+        logger.debug("Получение списка треков")
+        return await self.client.tracks(track_ids=track_ids)[offset:count] if track_ids \
+            else await self.client.users_likes_tracks()[offset:count]
+
+    async def download_tracks(self, track_list: TracksList,
+                              dir_name: str = "tracks",
+                              codec: str = 'mp3',
+                              bitrate: int = 192,
+                              count: int = 10,
+                              offset: int = 0,
+                              repeats: bool = False) -> None:
+        """Cохранение треков
 
         Args:
             dir_name (str): Имя каталога, куда следует сохранять треки. Каталог может быть только в каталогом
             с этим файлом.
             codec (str): Кодек скачиваемого трека. Известные значения  `mp3`, `aac`.
             bitrate (int): Битрейт скачиваемого трека. Известные значения 64`, `128`, `192`, `320`.
-            count (int): Количество треков, которые нужно скачать
-            offset (int): сдвиг
+            count (int): Количество треков, которые нужно скачать. 0, если нужно вывести все треки.
+            offset (int): Сдвиг.
+            repeats (bool): Нужно ли проверять на наличие повторяющихся треков (для загрузки на пк).
+            track_list (TracksList): объекты для загрузки.
         """
+
+        await self.__init_client()
 
         # Управляющая переменная
         counter = 0
 
         # Уже сохраненные треки
-        downloaded_tracks = get_downloaded_tracks(dir_name, codec)
+        downloaded_tracks = await get_downloaded_tracks(dir_name, codec) if not repeats else None
 
         # Имя папки может состоять только из цифр и букв
         dir_name = ''.join([i for i in dir_name if i.isalnum() or i.isspace()]) if dir_name != "tracks" else dir_name
@@ -151,63 +190,99 @@ class YandexMusic:
             logger.error("Указан неверный битрейт")
             return None
 
-        logger.debug("Получение списка понравившихся треков")
-        track_list = self.client.users_likes_tracks()  # Получение списка понравившихся треков
-
         for track in track_list[offset:]:
 
             # Выход из цикла по счетчику
-            if counter >= count:
+            if counter >= count and count:
                 break
 
             logger.debug("Получение информации о треке")
-            track = track.fetch_track()  # Получение информации о треке
+            track: Track = await track.fetch_track_async()  # Получение информации о треке
+            a = await track.get_specific_download_info_async(codec, bitrate)
+            print(a)
 
             # Имя трека может состоять только из цифр и букв
             track_name = ''.join([i for i in track["title"] if i.isalnum() or i.isspace()])
 
             # Проверка на повторы
-            if f"{track_name}.{codec}" in downloaded_tracks:
+            if f"{track_name}.{codec}" in downloaded_tracks and not repeats:
                 continue
 
             logger.debug("Сохранение трека")
             # Сохранение трека
-            track.download(filename=f"./{dir_name}/{track_name}.{codec}", codec=codec, bitrate_in_kbps=bitrate)
+            await track.download_async(filename=f"./{dir_name}/{track_name}.{codec}", codec=codec,
+                                       bitrate_in_kbps=bitrate)
 
             # Присвоение тегов
-            set_tags(track=track, file=f"./{dir_name}/{track_name}.{codec}")
+            await set_tags(track=track, file=f"./{dir_name}/{track_name}.{codec}")
 
-            counter += 1  # Увеличение счетчика
+            if count:
+                counter += 1  # Увеличение счетчика
 
 
 class Telegram:
-    """Упрощение взаимодействия с модулем yandex_music
+    """Упрощение взаимодействия с модулем telebot.
 
     Args:
-        token (str): Токен для авторизации в telegram
-        your_id (int): ID телеграм чата для отправки сообщений
+        token (str): Токен для авторизации в telegram.
     """
 
-    def __init__(self, token: str = TELEGRAM_TOKEN, your_id: int = TELEGRAM_ID):
+    def __init__(self, token: str = TELEGRAM_TOKEN) -> None:
         try:
-            self.your_id = your_id
-            self.bot = TeleBot(token)
+            self.bot = AsyncTeleBot(token)
             self.bot.get_me()
         except ValueError:
             logger.error("Указан неверный токен")
         except apihelper.ApiTelegramException:
             logger.error("Указан недействительный токен")
 
-    def send_audio(self, path):
-        """ Отправляет указанный трек в чат телеграм
+    async def send_audio_from_file(self, path, chat_id: int | str = TELEGRAM_ID) -> bool:
+        """ Отправляет указанный трек в чат телеграм.
 
         Args:
-            path (str): Путь до отправляемого файла
+            path (str): Путь до отправляемого файла.
+            chat_id (int | str): id получателя.
         """
+
         try:
-            self.bot.send_audio(self.your_id, open(path, 'rb'))
+            await self.bot.send_audio(chat_id, open(path, 'rb'))
             logger.debug("Трек отправлен")
             return True
         except Exception as e:
             logger.error("Ошибка при отправке аудио: ")
             return False
+
+    async def send_audio_from_link(self, track: Track, chat_id: int | str = TELEGRAM_ID) -> bool:
+        """ Отправляет указанный трек в чат телеграм.
+
+        Args:
+            track (str): Обьект класса Track.
+            chat_id (int | str): id получателя.
+        """
+        logger.debug("Получение загрузочной ссылки")
+        download_info = await track.get_specific_download_info_async(codec="mp3", bitrate_in_kbps=192)
+        direct_link = download_info.get_direct_link_async() if download_info.direct_link is None \
+            else download_info.direct_link
+
+        track_title = track.title
+        track_artist = ", ".join([i["name"] for i in track.artists])
+        track_cover = track.cover_uri
+
+        try:
+            await self.bot.send_audio(chat_id=chat_id, audio=direct_link,
+                                      performer=track_artist,
+                                      title=track_title,
+                                      thumbnail=track_cover)
+            logger.debug("Трек отправлен")
+            return True
+        except Exception as e:
+            logger.error("Ошибка при отправке аудио: ")
+            return False
+
+
+async def main():
+    a = YandexMusic()
+    await a.download_tracks(count=10)
+
+
+asyncio.run(main())
